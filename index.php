44946ae023c9353414b0105f4936f332a6700dfa
camille <camille_chen25@hotmail.fr>
2018-10-24 12:25:43 +0200

    Revert "archi"
    This reverts commit e631c7ad7fb269082feeb778a40af15f8f3d8426.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include_once("layouts/head"); ?>
+</head>
+<body>
+	<header>
+		//mon header
+		
+	</header>
+	<main>
+		//mon contenu
+		
+	</main>
+
+	<footer>
+		//mon footer
+		
+	</footer>
+	
+</body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include_once("layouts/head"); ?>
+</head>
+<body>
+	<header>
+		//mon header
+		
+	</header>
+	
+</body>
+</html>
